15050ed66313066b6eddb03a861f0d25728499b6
sergit <storrellas@gmail.com>
2016-01-21 09:52:29 +0100

    more commits

---
+++ admininterface/templatetags/tags.py
@@ -0,0 +1,20 @@
+# Django imports
+from django import template
+from django.conf import settings
+
+from django.core.urlresolvers import reverse
+
+
+
+register = template.Library()
+
+ 
+
+@register.inclusion_tag('sidebar.html', takes_context=True)
+def sidebar(context):
+    request = context['request']
+    url = request.path
+    
+    if reverse('base') == request.path:
+        pass
+    return {'url': "sergi" }
