713fb732106a5218cb70910d623e9424496e2ab2
sergit <storrellas@gmail.com>
2016-02-05 08:11:41 +0100

    Added fixture for token

--- install_fixtures.sh
+++ install_fixtures.sh
@@ -1,1 +1,1 @@
-./manage.py loaddata admininterface/fixtures/users.json admininterface/fixtures/initial.json
+./manage.py loaddata admininterface/fixtures/users.json admininterface/fixtures/authtoken.json admininterface/fixtures/initial.json
