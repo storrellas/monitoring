6d5d757d7d549993d72b4a1fe4e168308c9c21da
sergit <storrellas@gmail.com>
2016-02-05 08:00:08 +0100

    Repo corrupted

---
+++ samplingcontrol/localsettings.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'samplingcontrol',
+        'USER': 'sergi',
+        'PASSWORD': 'sergi',
+        'HOST': 'localhost'
+    }
+}
---
+++ testimports.py
@@ -0,0 +1,12 @@
+from apiv2.serializers import *
+from admininterface.models import *
+from django.contrib.auth.models import User, Group
+from django.db.models import Sum
+from rest_framework.renderers import JSONRenderer
+from django.utils.six import BytesIO
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
+
+user= User.objects.get(id=2)
+event=Event.objects.first()
+eventcheck = EventCheck.objects.first()
