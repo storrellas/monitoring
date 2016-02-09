98b3f362429fc17f0cda6090d724adc84ae43d93
sergit <storrellas@gmail.com>
2016-02-09 08:16:33 +0100

    Added SuperuserRequiredMixin

--- samplingcontrol/settings.py
+++ samplingcontrol/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'admininterface.middleware.CheckRole'
 )
 
 ROOT_URLCONF = 'samplingcontrol.urls'
