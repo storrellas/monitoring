22308758bdd8e79294a69de9bb08aee4f7b231b2
sergit <storrellas@gmail.com>
2016-02-11 09:39:53 +0100

    Removed localsettings

--- samplingcontrol/localsettings.py
+++
@@ -1,9 +0,0 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'samplingcontrol',
-        'USER': 'sergi',
-        'PASSWORD': 'sergi',
-        'HOST': 'localhost'
-    }
-}
