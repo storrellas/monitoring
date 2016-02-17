4fabf24a7b50071c8e7b6d74365f7fad6b558a7a
sergit <storrellas@gmail.com>
2016-02-17 09:12:34 +0100

    Added debug

--- apiv2/serializers.py
+++ apiv2/serializers.py
@@ -110,6 +110,11 @@ class EventCheckAppSerializer(serializers.ModelSerializer):
         # Return total samplings for the given user and event
         try:
             user = self.context['request'].user    
+            print "Debugging here"
+            print obj.event
+            print user
+            print datetime.now()
+            
             list = EventCheck.objects.filter(event=obj.event, user=user)
             print list
             total_dict=EventCheck.objects.filter(event=obj.event, user=user,trackdate=datetime.now()) \
